f68a8a3cee40e5b62125f9b0ad2c73d6c5209ed4
AnberYuan <yuan.linghui@towngas.com.cn>
2020-12-14 15:27:43 +0800

    cute

---
+++ bin/program.js
@@ -0,0 +1,15 @@
+// 配置cute-cli的一些命令
+const program = require('commander');
+const { SSL_OP_NO_TLSv1_1 } = require('constants');
+const path = require('path');
+const packageData = require(path.resolve(__dirname, '../package.json'));
+
+program
+  .version(packageData.version, '-v, --vers', 'output the current version')
+  .option('-d, --dev [devConf]', '开发环境启动')
+  .option('-b, --build [buildConf]', '生产环境编译')
+  .option('-i, --init <projectName>', '生成基本项目')
+
+program.parse(process.argv);
+
+module.exports = program;
